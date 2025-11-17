cbce5c4bb0d6f272b5674e7e79f84adf1e21e9a9
Vinh Ga <nguyenngocvinh770@gmai.com>
2025-11-18 00:01:22 +0700

    Ơ[#4] Css Baseline

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import App from './App.jsx'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import CssBaseline from '@mui/material/CssBaseline'
+
 
 createRoot(document.getElementById('root')).render(
-  <App/>
+  <>
+    <CssBaseline />
+    <App/>
+  </>
 )
