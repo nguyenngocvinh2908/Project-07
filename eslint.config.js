1037bd1e1ef8feb17edacf56e17a5dcaf3b8f1af
Vinh Ga <nguyenngocvinh770@gmai.com>
2025-11-17 20:21:00 +0700

    Initial Project

---
+++ eslint.config.js
@@ -0,0 +1,52 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module'
+      }
+    },
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+
+      'no-console': 'warn',
+      'no-lonely-if': 'warn',
+      'no-unused-vars': 'warn',
+      'no-trailing-spaces': 'warn',
+      'no-multi-spaces': 'warn',
+      'no-multiple-empty-lines': 'warn',
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': ['warn', 'always'],
+      'indent': ['warn', 2],
+      'semi': ['warn', 'never'],
+      'quotes': ['error', 'single'],
+      'array-bracket-spacing': 'warn',
+      'linebreak-style': 'off',
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 'warn',
+      'comma-dangle': 'warn',
+      'comma-spacing': 'warn',
+      'arrow-spacing': 'warn'
+    }
+  }
+])
